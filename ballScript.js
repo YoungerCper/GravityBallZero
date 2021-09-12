64d756eb1f8a4a44e681c6d84b7a67a02f53f314
Andrew <83809296+YoungerCper@users.noreply.github.com>
2021-09-12 15:02:24 +0300

    Zero_0_1

--- ballScript.js
+++ ballScript.js
@@ -6,50 +6,72 @@ const w = {
     width: document.documentElement.clientWidth,
     height: document.documentElement.clientHeight,
     k: 0.8
-}
+};
+
+const ballFactory = {
+    ballArray: [],
+
+    Factory(we, posX, posY)
+    {
+
+        const bodySpace = document.getElementById("main");
+        const e = document.createElement('div');
+
+        e.className = 'ball';
+        e.style.top = posY;
+        e.style.left = posX;
+
+        bodySpace.appendChild(e);
+
+        const newBall = {
+            element: e,
+            weight: we,
+            xpos: 50,
+            ypos: 50,
+            speedX: 0,
+            speedY: 0,
+            Draw()
+            {
+                this.element.style.top = this.ypos + 'px';
+                this.element.style.left = this.xpos + 'px';
+            },
+
+            Move()
+            {
+                
+                this.xpos += this.speedX + w.gX / 2;
+                this.ypos += this.speedY + w.gY / 2;    
+                this.speedY += w.gY;
+                this.speedX += w.gX;
+                if(this.xpos + 100 >= w.width || this.xpos <= 0)
+                { 
+                    this.speedX = (this.xpos <= 0) ? Math.floor(-this.speedX * w.k) : Math.ceil(-this.speedX * w.k);
+                    this.xpos = (this.xpos <= 0) ? 0 : w.width - 100; 
+                }
+
+                if(this.ypos + 100 >= w.height || this.ypos <= 0)
+                {
+                    this.speedY = (this.ypos <= 0) ? Math.floor(-this.speedY * w.k) : Math.ceil(-this.speedY * w.k);
+                    this.ypos = (this.ypos <= 0) ? 0 :  w.height - 100;
+                }        
+            
+            }
+        };
+
+        this.ballArray.push(newBall);
+    },
+
+    Work()
+    {
+        for(let i = 0; i < this.ballArray.length; i++)
+        {
+            this.ballArray[i].Draw();
+            this.ballArray[i].Move();
+        }
+    }
+};
 
 alert(w.height);
-
-function BallCreator(we, posX, posY)
-{
-
-    let e = document.getElementById('ball');
-
-    return {
-        element: e,
-        weight: we,
-        xpos: 50,
-        ypos: 50,
-        speedX: 0,
-        speedY: 0,
-        Draw()
-        {
-            this.element.style.top = this.ypos + 'px';
-            this.element.style.left = this.xpos + 'px';
-        },
-
-        Move()
-        {
-            
-            this.xpos += this.speedX + w.gX / 2;
-            this.ypos += this.speedY + w.gY / 2;    
-            this.speedY += w.gY;
-            this.speedX += w.gX;
-            if(this.xpos + 100 >= w.width || this.xpos <= 0)
-            { 
-                this.speedX = (this.xpos <= 0) ? Math.floor(-this.speedX * w.k) : Math.ceil(-this.speedX * w.k);
-                this.xpos = (this.xpos <= 0) ? 0 : w.width - 100; 
-            }
-
-            if(this.ypos + 100 >= w.height || this.ypos <= 0)
-            {
-                this.speedY = (this.ypos <= 0) ? Math.floor(-this.speedY * w.k) : Math.ceil(-this.speedY * w.k);
-                this.ypos = (this.ypos <= 0) ? 0 :  w.height - 100;
-            }        
-        
-        }
-    };
-}
 
 function KeyReader(e)
 {
@@ -77,15 +99,13 @@ function KeyReader(e)
 
 function main()
 {
-    
-    ballObj.Move();
-    ballObj.Draw()
+    //alert(w.width);
+    ballFactory.Work()
     //alert(w.width);
     requestAnimationFrame(main);
 }
 
-
 document.addEventListener("keydown", KeyReader);
-const ballObj = BallCreator(1, 10, 10);
+ballFactory.Factory(1, 10, 10);
 
 main();
